27460a65af73d6bedbb93f2ebe53ad046af6e84f
MikeMakdesi <toppbetygab@gmail.com>
2021-09-15 22:38:56 +0200

    Plagrism checker

---
+++ assets/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAIgtdZt5f9icRLGRlvifLnNAJZsaLicZ4",
+    authDomain: "plagiarism-checker-95ca8.firebaseapp.com",
+    projectId: "plagiarism-checker-95ca8",
+    storageBucket: "plagiarism-checker-95ca8.appspot.com",
+    messagingSenderId: "319622426089",
+    appId: "1:319622426089:web:ab206c7632da153c502f78"
+  };
